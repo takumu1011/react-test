ea9ed0f8aa18629d9b35be8351ea15750ade5698
tkm <sumikawa.web@gmail.com>
2020-09-12 22:40:40 +0900

    props

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Hello name="React" />
   </React.StrictMode>,
   document.getElementById('root')
 );
